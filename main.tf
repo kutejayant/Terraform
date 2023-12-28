b6f9081a653b090f1b560805d5d7fac33cadd0af
root <root@Jayant>
2023-12-29 00:14:45 +0530

    4th commit for main.tf

--- main.tf
+++ main.tf
@@ -16,26 +16,29 @@ resource "aws_vpc" "my_vpc" {
   cidr_block = "10.0.0.0/16"   
 }
 
- variable "public_subnets_cidr_block" {
-    default = ["10.0.5.0/24", "10.0.6.0/24"]
-}
+#resource "aws_subnet" "public_subnets_cidr_blocks" {
+#   count = element(var.public_subnets_cidr_block.id) 
+#   vpc_id = aws.vpc.my_vpc.id 
+#   cidr_block = element(var.public_subnets_cidr_blocks, count.index)
+#}
 
-  variable "private_subnets_cidr_block" {
-    default = ["10.0.7.0/24", "10.0.8.0/24"]
-}
+#resource "aws_subnet" "private_subnets_cidi_blocks" {
+#   count = element(var.private_subnets_cidr_block.id) 
+#   vpc_id = aws.vpc.my_vpc.id 
+#   cidr_block = element(var.private_subnets_cidr_block, count.index)
+#}
 resource "aws_subnet" "public_subnets" {
-  count     = length( var.public_subnets_cidr_block)
+  count     = length(var.public_subnets_cidr_blocks)
   vpc_id    = aws_vpc.my_vpc.id
-  cidr_block = var.public_subnets_cidr_block[count.index]
+  cidr_block = element ( var.public_subnets_cidr_blocks, count.index)
    tags = {
     "Name" = "public_subnet ${count.index + 1}"
   }
 }
 resource "aws_subnet" "private_subnets" {
-  count     = length( var.private_subnets_cidr_block)
+  count     = length (var.private_subnets_cidr_blocks)
   vpc_id    = aws_vpc.my_vpc.id
-  cidr_block= element (var.private_subnets_cidr_block, count.index)
- # cidr_block = var.private_subnets_cidr_block[count.index]
+  cidr_block= element(var.private_subnets_cidr_blocks, count.index)
     tags = {
     "Name" = "private_subnet ${count.index + 1}"
   }
@@ -45,7 +48,6 @@ resource "aws_internet_gateway" "my_igw" {
 }
 
 resource "aws_route_table" "public_rt" {
- # count  = length(aws_subnet.public_subnets)
   vpc_id = aws_vpc.my_vpc.id
 
   route {
@@ -57,7 +59,6 @@ resource "aws_route_table" "public_rt" {
   }
 }
 resource "aws_route_table" "private_rt" {
-  #count  = length(aws_subnet.private_subnets)
   vpc_id = aws_vpc.my_vpc.id
 
   route {
@@ -72,9 +73,7 @@ resource "aws_eip" "my_eip" {
   domain =  "vpc"
 }
  resource "aws_nat_gateway" "my_ngw" {
-  #count = length(aws_subnet.public_subnets)
   allocation_id = aws_eip.my_eip.id
- # subnet_id     = element (aws_subnet.public_subnets[*].id ,0 )
  subnet_id = aws_subnet.public_subnets[0].id
   
   tags = {
@@ -82,13 +81,13 @@ resource "aws_eip" "my_eip" {
   }
 }
 resource "aws_route_table_association" "private_associations" {
-  count         = length( var.private_subnets_cidr_block)
+  count         = length( var.private_subnets_cidr_blocks)
   subnet_id     = element (aws_subnet.private_subnets[*].id, count.index)
   route_table_id =  aws_route_table.private_rt.id
 }
 
 resource "aws_route_table_association" "public_associations" {
-  count         = length( var.public_subnets_cidr_block)
+  count         = length( var.public_subnets_cidr_blocks)
   subnet_id     = element (aws_subnet.public_subnets[*].id,count.index )
   route_table_id =  aws_route_table.public_rt.id 
 }
